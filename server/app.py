2f407787bc98b3e4a9b84f34072d1434dcb32c89
abhi <abhisrinath@gmail.com>
2019-07-26 08:19:41 -0700

    initial commit for getting document summaries + small refactor

--- server/app.py
+++ server/app.py
@@ -16,16 +16,18 @@ def index():
 
 @route('/agora/post/create/<category>/v1', method='POST')
 def create_post(category):
-    body = request.json
-    return json.dumps({'success': post.insert_one(body, collection=category)})
+    return json.dumps({'success': post.insert_one(request.json, collection=category)})
 
 
 @route('/agora/image/<category>/upload/v1', method='POST')
 def upload_images(category):
     file_obj = request.files.get('upload')
-    image_like = file_obj.file
-    response = upload_images_to_s3([image_like], category)
-    return response
+    return upload_images_to_s3([file_obj.file], category)
+
+
+@route('/agora/posts/<category>/summary/v1', method='POST')
+def get_matching_posts(category):
+    return post.get_many(request.json, collection=category)
 
 
 @route('/agora/post/<category>/<post_id>/details/v1', method='GET')
